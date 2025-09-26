001684da8db3228d977e75c0c6eea00b60d8e9fd
abdlismailca <abdlismailca@gmail.com>
2025-09-26 07:11:59 +0530

    js updated- pos

--- point_of_sale_rating/static/src/js/rating.js
+++ point_of_sale_rating/static/src/js/rating.js
@@ -1,57 +1,29 @@
-/** @odoo-module */
-
-
+/** @odoo-module **/
 
 import { patch } from "@web/core/utils/patch";
+import { Product } from "@point_of_sale/app/store/product";
+import { ProductList } from "@point_of_sale/app/screens/product_screen/product_list/product_list";
 import { ProductCard } from "@point_of_sale/app/generic_components/product_card/product_card";
 
+patch(Product.prototype, {
+    setup() {
+        this._super(...arguments);
+        console.log("Loaded product with rating:", this.display_name, this.quality_rating);
+    },
+});
+
+patch(ProductList.prototype, {
+    getProductProps(product) {
+        return {
+            ...super.getProductProps(product),
+            quality_rating: product.quality_rating,
+        };
+    },
+});
 
 patch(ProductCard, {
     props: {
         ...ProductCard.props,
-        qty: {type: Number, optional: true},
+        quality_rating: { type: String, optional: true },
     },
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-//import { registry } from "@web/core/registry";
-
-
-
-
-//import { usePos } from "@point_of_sale/app/store/pos_hook";
-//import { Component } from "@odoo/owl";
-//
-//
-//import { patch } from "@web/core/utils/patch";
-//import { PosStore } from "@point_of_sale/app/store/pos_store";
-//
-//
-////patch(PosStore,prototype,{
-////   _loadProductProduct(products) {
-////    var self = this;
-////    super._loadProductProduct(...arguments);
-////    for(const prod of products){
-////
-////    self.db.product_by_id[prod.id].quality_rating =  prod.quality_rating
-////
-////    }
-////
-////   }
-////
-////})
-//
-//
-//
-//
+});
