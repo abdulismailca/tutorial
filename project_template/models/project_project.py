8a1999c96e8e4656728f497c32fa2728e8638169
abdlismailca <abdlismailca@gmail.com>
2025-10-15 00:25:42 +0530

    client

--- project_template/models/project_project.py
+++ project_template/models/project_project.py
@@ -6,11 +6,20 @@ class ProjectProject(models.Model):
     _inherit = "project.project"
 
     project_template_id = fields.Many2one("project.template", string="Project template")
+    is_button_view = fields.Boolean(string="Is button view")
 
 
     @api.onchange('project_template_id')
     def link_project_template(self):
        tag_id_list = self.mapped('project_template_id.tag_ids')
+
+       # task_id_list = self.project_template_id.search([])
+       # for task in self.project_template_id.task_ids:
+       #     task_id_list.append(Command.create({
+       #         'name': task.name,
+       #     }))
+
+       # print('from from project sub',task_id_list)
 
 
        self.write({'name':self.project_template_id.name})
@@ -18,6 +27,15 @@ class ProjectProject(models.Model):
        self.write({'user_id': self.project_template_id.user_id})
        self.write({'description': self.project_template_id.description})
        self.write({'tag_ids':tag_id_list})
+       self.write({'partner_id':self.project_template_id.partner_id})
+       self.write({'company_id':self.project_template_id.company_id})
+       # self.update({
+       #     'task_ids': [(fields.Command.set(id))]
+       # })
+       # self.update({
+       #     'task_ids': [(fields.Command.set(a.id)) for a in task_id_list]
+       # })
+
 
 
 
@@ -26,8 +44,17 @@ class ProjectProject(models.Model):
 
     def create_project_template(self):
         tag_ids_list = self.mapped('tag_ids')
+        task_ids_list = []
 
-        print("tag ids",tag_ids_list)
+        for task in self.task_ids:
+            task_ids_list.append (Command.create({
+                'name': task.name,
+            }))
+
+        self.is_button_view = True
+
+        print("its task ids",self.task_ids)
+
 
         self.env['project.template'].create({
             'name':self.name,
@@ -36,11 +63,7 @@ class ProjectProject(models.Model):
             'user_id': self.user_id.id,
             'description': self.description,
             'tag_ids': tag_ids_list,
+            'company_id':self.company_id.id,
+         })
 
-        })
 
-    # label_task = fields.Char(string="Task Label")
-    # partner_id = fields.Many2one("res.partner", string="Partner")
-    # tag_ids = fields.Many2many("project.tags", string="project Tags")
-    # user_id = fields.Many2one("res.users", string="project Manager")
-    # description = fields.Html(string="Description")
--- project_template/models/project_template.py
+++ project_template/models/project_template.py
@@ -1,4 +1,4 @@
-from odoo import fields,models
+from odoo import fields, models, Command
 
 
 class ProjectTemplate(models.Model):
@@ -12,3 +12,30 @@ class ProjectTemplate(models.Model):
     tag_ids = fields.Many2many("project.tags", string="project Tags", tracking=True)
     user_id = fields.Many2one("res.users",string="project Manager", tracking=True)
     description = fields.Html(string="Description", tracking=True)
+    company_id = fields.Many2one("res.company", string="Company", tracking=True)
+    task_ids = fields.One2many("project.task",'project_id', string="Tasks", tracking=True, default=None)
+
+    def create_project_from_temp(self):
+
+        tag_ids_list = self.mapped('tag_ids')
+        task_ids_list = []
+
+        for task in self.task_ids:
+            task_ids_list.append(Command.create({
+                'name': task.name,
+            }))
+
+        print("its task ids from temp", self.task_ids)
+
+        self.env['project.project'].create({
+            'name': self.name,
+            'label_tasks': self.label_tasks,
+            'partner_id': self.partner_id.id,
+            'user_id': self.user_id.id,
+            'description': self.description,
+            'tag_ids': tag_ids_list,
+            'company_id': self.company_id.id,
+            'task_ids': task_ids_list,
+
+        })
+
