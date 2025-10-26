fa33d05efdebabbc7ae24bac966e6369484e85f6
abdlismailca <abdlismailca@gmail.com>
2025-10-26 21:23:13 +0530

    Done

--- payment_multisafepay/models/__init__.py
+++ payment_multisafepay/models/__init__.py
@@ -1,2 +1,7 @@
-from . import payment_provider
-from . import payment_transaction
+# from . import project_project
+# from . import payment_provider
+# from . import sale_order
+# from . import stock_picking
+# from . import sale_order_cost
+# from . import when_confirm_sale_order
+# from . import sale_order_project_task
